127c8f5d8a897bc9c8340c600b07918ca31ff187
PhatJack <tienphat.ng693@gmail.com>
2025-12-12 09:36:35 +0700

    refactor code structure for improved readability and maintainability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,19 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt([
+  // Disable ESLint rules that conflict with Prettier
+  eslintConfigPrettier,
+  // Add Prettier as an ESLint rule
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      ...eslintPluginPrettier.configs.recommended.rules,
+      'prettier/prettier': 'error',
+    },
+  },
+])
